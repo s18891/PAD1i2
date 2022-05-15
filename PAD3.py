3e4bbb8fdca987eacfc8c2bcfc2fc3ac3340d142
Bogusław Stępień <s18891@pjwstk.edu.pl>
2022-05-15 23:25:54 +0200

    pad3

--- PAD3.py
+++ PAD3.py
@@ -1,9 +1,35 @@
 import pandas as pd
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import string
 
 if __name__ == '__main__':
-    sf_sal = pd.read_csv('PAD_03_PD.csv')
+    df = pd.read_csv('PAD_03_PD.csv', sep=';')
 
+    print(df)
+
+    print(df.head())
+
+    print(df.columns)
+
+    print(df[["Country"]])
+
+
+    zad1 = df[['Country']].groupby(['Country']).nunique()
+    zad1[['Count']] = df[['ID']].groupby(df['Country']).nunique()
+    print(zad1[['Count']])
+
+    zad2 = df
+    zad2["owned_goods"] = df["owns_car"] + df["owns_TV"] + df["owns_house"] + df["owns_Phone"]
+
+    print(zad2[['owned_goods']])
+
+    zad3 = zad2.groupby(['gender']).mean().round(2)
+
+    print(zad3[["owned_goods"]])
+
+
+    zad4 = zad2[['Country']].groupby(['Country']).nunique()
+    zad4[['avg_goods']]= zad2.groupby(['Country'])[["owned_goods"]].mean().round(2)
+
+
+
+    print(zad4)

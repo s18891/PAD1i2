d0fd2923a00af10a2e09cacb980b2e8e4a0fadd3
Boguś <s18891>
2022-07-01 14:37:10 +0200

    pad4. całe

--- PAD4.py
+++ PAD4.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import pandas as pd
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -44,6 +44,26 @@ if __name__ == '__main__':
     print(np.linalg.inv(matrix2))
 
 
+#zad3
+print("zad3")
+a= np.genfromtxt('Zadanie_3_macierz_A.csv', delimiter=',')
+b= np.genfromtxt('Zadanie_3_macierz_B.csv', delimiter=',')
+print(a.dot(b.transpose())/(np.std(a)*np.std(b)))
+
+#zad4
+print("zad4")
+
+
+df= pd.read_csv('Zadanie_4.csv', sep=';')
+df['Hour'] = pd.to_datetime(df['DateTime']).dt.hour
+df['Date'] = pd.to_datetime(df['DateTime']).dt.date
+df['Minute'] = pd.to_datetime(df['DateTime']).dt.minute
+
+result = df[["DoctorID",'Type',"City","Date",'Hour']].groupby(['DoctorID','Type',"City","Date","Hour"]).nunique()
+result[["First Minute"]] = df.groupby(["DoctorID",'Type',"City","Date","Hour"])[["Minute"]].first()
+result[["Last Minute"]] = df.groupby(["DoctorID",'Type',"City","Date","Hour"])[["Minute"]].last()
+result.to_csv("result.csv", sep=";")
+print(result)
 
 
 
